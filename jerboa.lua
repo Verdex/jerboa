b26862706613d11f4350bbb540d23b5a7c519177
David L. Inman <davidlevere@gmail.com>
2019-12-05 00:35:11 -0500

    working on constructor parser

--- jerboa.lua
+++ jerboa.lua
@@ -26,6 +26,68 @@ function lua_data_to_string(data)
     end
 end
 
+-- $1, Var, symbol, symbol(<<paramList>>), <<paramList>> = this
+function parse_constructor(str, index)
+    local top_level = not index
+    local sub = string.sub 
+    local match = string.match
+    index = index or 1
+    function clear_whitespace()
+        while match( sub(str, index, index), "%s" ) do
+            index = index + 1
+        end
+    end
+
+    clear_whitespace()
+    if sub(str, index, index) == "$" then
+        local number, end_pos = match(sub(str, index), "(%d+)()")
+        index = end_pos 
+        clear_whitespace()
+        if top_level and index <= #str then
+            error( "found additional characters")
+        elseif top_level then
+            return { t = 'data', k = 'cap', number = tonumber(number) }
+        end
+        return { t = 'data', k = 'cap', number = tonumber(number) }, index
+    elseif match( sub(str, index, index), "%u" ) then
+        local name, end_pos = match(sub(str, index), "(%w+)()")
+        index = end_pos 
+        clear_whitespace()
+        if top_level and index <= #str then
+            error( "found additional characters")
+        elseif top_level then
+            return { t = 'data', k = 'var', name = name }
+        end
+        return { t = 'data', k = 'var', name = name }, index
+    elseif match( sub(str, index, index), "%l" ) then
+        local name, end_pos = match(sub(str, index), "(%w+)()")
+        index = end_pos 
+        clear_whitespace()
+        if index > #str then
+            return { t = 'data', k = 'sym', name = name }, index
+        end
+        if sub(str, index, index) == "(" then
+            local params = {}
+            while sub(str, index, index) ~= ")" do
+                params[#params+1], index = parse_constructor(str, index + 1)
+                local n = sub(str, index, index)
+                if n ~= ',' and n ~= ')' then
+                    error( "expected comma in param list" )
+                end
+            end
+            clear_whitespace()
+            if top_level and index <= #str then
+                error( "found additional characters")
+            elseif top_level then
+                return { t = 'data', k = 'fun', name = name, params = params }
+            end
+            return { t = 'data', k = 'fun', name = name, params = params }, index
+        end 
+        error( "found additional characters")
+    else
+        error( "Encountered unknown data element: " .. sub( str, index, index ) .. " : " .. index )
+    end
+end
 --[[
 lex blah {
     "regex_string" => Data($1, $2);
@@ -33,4 +95,5 @@ lex blah {
 }
 
 --]]
+
 
