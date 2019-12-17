a228bfbc7b9f56bd550e5ce7ec37ee627d8a94a7
David L. Inman <davidlevere@gmail.com>
2019-12-17 00:47:08 -0500

    todos

--- src/main.ml
+++ src/main.ml
@@ -2,13 +2,14 @@
 open Base
 
 (* todo *)
+(* data_to_lexer, data_to_parser *)
 let to_eval (input:data) : string = "todo"
 
 (* todo *)
 let lex (l : lexer) (input : string) : data = Atom("todo", {start_index = 0; end_index = 0})
 (* automatically prune whitespace  *)
 
-(* find_lexer, find_parser, find_rule, match_pattern *)
+(* find_lexer, find_parser, find_rule, match_pattern, construct *)
 
 (* todo *)
 let parse (lexers : lexer list) 
