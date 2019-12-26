d9e7fa01a7efc71c9706edfe71f18600cd1c7588
David L. Inman <davidlevere@gmail.com>
2019-12-26 17:13:20 -0500

    working on a constructor function

--- src/main.ml
+++ src/main.ml
@@ -7,8 +7,19 @@ exception LexError of int
 (* data_to_lexer, data_to_parser *)
 let to_eval (input:data) : string = "todo"
 
+let rec construct (c : constructor) (captures: data list) (var_env : (string * data) list) : data = 
+(* first of first capture and last of last caputure for start and end index *)
+    match c with
+    | Atom(name) -> Atom(name, {start_index = 0; end_index = 0} ) 
+    | Fun(name, params) -> Fun( name 
+                              , List.map (fun param -> construct param captures var_env) params
+                              , {start_index = 0; end_index = 0} 
+                              )
+    | CaptureRef(ref_nums) -> Atom("blah", {start_index = 0; end_index = 0}) 
+    | Var(name) -> Atom("blah", {start_index = 0; end_index = 0})
+
 (* todo *)
-let lex (lexer : lexer) (input : string) : data = 
+let lex (lexer : lexer) (input : string) : data list = 
     let is_whitespace c = c = ' ' || c = '\t' || c = '\r' || c = '\n' in
 
     let gen_rules (Lexer (_, rules)) : (Str.regexp * constructor) list = 
@@ -37,11 +48,11 @@ let lex (lexer : lexer) (input : string) : data =
         else
             let (result, new_index) = try_rules input !index in 
             match result with
-            | Some( (value, cons) ) -> output := value :: !output ; index := new_index
+            | Some( (value, cons) ) -> output := (value, cons, !index, new_index) :: !output ; index := new_index
             | None -> raise (LexError !index)
     done 
-    ; List.iter (fun x -> Printf.printf "%s\n" x) (List.rev !output)
-    ; Atom("todo", {start_index = 0; end_index = 0})
+    ; List.iter (fun (x,_,_,_) -> Printf.printf "%s\n" x) (List.rev !output)
+    ; List.map (fun (v, c, s, e) -> construct c [String(v, {start_index = s; end_index = e})] []) (List.rev !output)
 
 (* find_lexer, find_parser, find_rule, match_pattern, construct *)
 
