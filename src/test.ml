0840a332a735d51d79dfecfa6d5a8423b8cfed9e
David L. Inman <davidlevere@gmail.com>
2020-01-27 22:13:20 -0500

    test methods

--- src/test.ml
+++ src/test.ml
@@ -4,4 +4,21 @@ open Lexer
 open Parser
 open Debug
 
+exception TestFailure of string
+
+let test (name : string) (t : unit -> unit) =
+    try 
+        t () 
+        ; Printf.printf "%s\n" (String.concat "" [name ; ": pass"])
+    with 
+    | TestFailure v -> Printf.printf "%s\n" (String.concat "" [name ; ": failed with message " ; v])
+    | _ -> Printf.printf "%s\n" (String.concat "" [name ; ": failed with exception"])
+
+
+let expect_eq (a : 'a) (b : 'a) (s : string) =
+    match a = b with
+    | true -> ()
+    | false -> raise (TestFailure s)
+
+;;
 
