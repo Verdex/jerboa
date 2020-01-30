3595c08239aaed1da11ad156fbba7b452ca74db6
David L. Inman <davidlevere@gmail.com>
2020-01-29 22:34:01 -0500

    parser test with single rule, single case, single atom pattern

--- src/test_parser.ml
+++ src/test_parser.ml
@@ -7,6 +7,33 @@ open Debug
 
 ;; 
 
-test "" (fun _ ->
-    Printf.printf "blah"
+test "parser with single rule single case and single atom pattern" (fun _ ->
+    let lexer : lexer = Lexer( "lex", [Rule( "[a]", Atom "A" )] ) in
+    let parser : parser = Parser( "parse", [Rule( "main", [Case( [Atom "A"], Atom( "output" ) )] ) ] ) in
+
+    let tokens = lex lexer "a" in
+    let output = parse [lexer] [parser] tokens "parse" in
+
+    let Success(Some(data), _) = output in
+
+    expect_eq data (Atom( "output", meta 0 0 )) "should produce atom output"
 )
+
+(* 
+    wild card
+    variable
+    capture group reference
+    nested capture group reference
+    parse reference
+    rule reference
+    atom match
+    function match
+    function match with atom
+    function match with multiple atoms
+    function match with group reference (also nested)
+    function match with parse reference
+    function match with rule reference
+    multiple cases
+    nothing pattern match
+    nothing construction
+*)
